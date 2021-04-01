1433589b33936a794d200aace054d6dae13ead48
raravel <youn@tree-some.dev>
2021-04-01 18:55:53 +0900

    add Posting header

--- src/views/Posting/Posting.vue
+++ src/views/Posting/Posting.vue
@@ -5,36 +5,43 @@
  * Copyright (c) git-story. Licensed under the GPL 3.0 License.
 -->
 <template>
-	<v-row class="mx-0 vh-100" style="background-color: #f2f4f7;">
-		<v-col class="d-none d-sm-block py-0" sm="1"  md="2" lg="3"></v-col>
-		<v-col class="py-0" cols="12" sm="10" md="8" lg="6">
-			<v-row style="background-color: white;" class="ma-0 h-100">
-				<v-col cols="12">
-					<v-row class="mx-0 pt-5 px-5">
-						<v-col cols="12">
-							<input type="text" class="title-input" spellcheck="false" :placeholder="$t('title')">
-						</v-col>
-					</v-row>
-					<v-divider class="mx-8" />
-					<v-row class="mx-0 pt-4 px-6" style="height: 90%; cursor: text;" @click="editorFocusEnd">
-						<v-col cols="12">
-							<MarkdownEditor ref="editor" v-model="markdown"/>
-						</v-col>
-					</v-row>
-				</v-col>
-			</v-row>
-		</v-col>
-		<v-col class="d-none d-sm-block py-0" sm="1"  md="2" lg="3"></v-col>
-	</v-row>
+	<v-main style="overflow: hidden;">
+		<Header />
+		<v-row class="mx-0" style="background-color: #f2f4f7; height: calc(100vh - 64px); overflow-y: auto;">
+			<v-col class="d-none d-sm-block py-0" sm="1"  md="2" lg="3"></v-col>
+			<v-col class="py-0" cols="12" sm="10" md="8" lg="6">
+				<v-row style="background-color: white; height: calc(100% - 3px);" class="ma-0 editor-shadow">
+					<v-col cols="12">
+						<v-row class="mx-0 pt-5 px-5">
+							<v-col cols="12">
+								<input type="text" class="title-input" spellcheck="false" :placeholder="$t('title')">
+							</v-col>
+						</v-row>
+						<v-divider class="mx-8" />
+						<v-row class="mx-0 pt-4 px-6" style="height: 90%; cursor: text;" @click="editorFocusEnd">
+							<v-col cols="12">
+								<div @click.stop="">
+									<MarkdownEditor ref="editor" v-model="markdown"/>
+								</div>
+							</v-col>
+						</v-row>
+					</v-col>
+				</v-row>
+			</v-col>
+			<v-col class="d-none d-sm-block py-0" sm="1"  md="2" lg="3"></v-col>
+		</v-row>
+	</v-main>
 </template>
 <script lang="ts">
 import { Component, Mixins } from 'vue-property-decorator';
 import GlobalMixins from '@/plugins/mixins';
 import MarkdownEditor from '@voraciousdev/vue-markdown-editor';
+import Header from './PostingHeader.vue';
 
 @Component({
 	components: {
 		MarkdownEditor,
+		Header,
 	},
 })
 export default class Posting extends Mixins(GlobalMixins) {
@@ -46,9 +53,9 @@ export default class Posting extends Mixins(GlobalMixins) {
 	}
 
 	public editorFocusEnd() {
-		console.log('click', this.$refs.editor);
-		this.$refs.editor.focus();
-		this.$refs.editor.focusEnd();
+		const editor = this.$refs.editor as any;
+		editor.focus();
+		editor.focusEnd();
 	}
 
 }
@@ -60,11 +67,9 @@ export default class Posting extends Mixins(GlobalMixins) {
 .title-input:focus {
 	outline: none;
 }
-.editor-pending {
-	cursor: text;
-	min-height: 1rem;
-	height: 2rem;
-	width: 100%;
-	flex-grow: 1;
+.editor-shadow {
+	-webkit-box-shadow: 0px 0px 18px 2px rgba(173,173,173,1);
+	-moz-box-shadow: 0px 0px 18px 2px rgba(173,173,173,1);
+	box-shadow: 0px 0px 18px 2px rgba(173,173,173,1);
 }
 </style>
---
+++ src/views/Posting/PostingHeader.vue
@@ -0,0 +1,35 @@
+<!--
+ * Header.vue
+ * Created on Thu Apr 01 2021
+ *
+ * Copyright (c) git-story. Licensed under the GPL 3.0 License.
+-->
+<template>
+	<v-app-bar
+		elevation="2"
+		color="indigo lighten-3">
+		<v-app-bar-nav-icon color="white"/>
+		<v-toolbar-title>
+			<router-link style="text-decoration: none" class="white--text" to="/">
+				GIT STORY
+			</router-link>
+		</v-toolbar-title>
+
+		<v-spacer />
+
+		<v-btn icon dark>
+			<v-icon>mdi-magnify</v-icon>
+		</v-btn>
+	</v-app-bar>
+</template>
+<script lang="ts">
+import { Component, Mixins } from 'vue-property-decorator';
+import GlobalMixins from '@/plugins/mixins';
+
+@Component({
+	components: {
+	},
+})
+export default class Header extends Mixins(GlobalMixins) {
+}
+</script>
