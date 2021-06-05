454cb20a47930c353dececf6e90af38f47acb1fa
Ash <ashkim2001@gmail.com>
2021-06-05 16:24:58 +0900

    english translation

---
+++ src/languages/en/home/index.ts
@@ -0,0 +1,36 @@
+/*
+ * index.ts
+ * Created on Thu Jul 16 2020
+ *
+ * Copyright (c) git-story. Licensed under the GPL 3.0 License.
+ */
+
+export default {
+	login: {
+		'id': 'ID',
+		'email': 'Email',
+		'pw': 'Password',
+		'login': 'Login',
+		'baner': 'What happened today?',
+		'desc': 'Git Story is a simple Git Blog management platform.<br>Share your story!',
+		'sign-in': 'Login using Github',
+	},
+	des1: {
+		title: 'Post anytime and anywhere!',
+		content: 'If you have a PC or a mobile device with internet connection, you can create a post without having to download Git and the repository.',
+	},
+	des2: {
+		title: 'Worry-free high volume traffic.',
+		content: 'Github Page can handle up to 100GB of traffic per month.<br>Meaning, it can handle over 10 thousand requests per day.<br>(300KB per page)',
+	},
+	des3: {
+		title: 'The best creation tool for building a static web.',
+		content: 'Hexo is faster than any other static web creation tools.<br>Also has many themes to decorate your blog, or you can custom build your own theme.',
+	},
+	foot: {
+		title: 'We will prepare, so just write!',
+		content: 'With a few clicks, finish preparing your blog and the design of it as well.<br>What story do you want to share?',
+		btn: 'Prepare a story',
+	},
+};
+ 
---
+++ src/languages/en/register/index.ts
@@ -0,0 +1,17 @@
+/*
+ * index.ts
+ * Created on Tue Aug 04 2020
+ *
+ * Copyright (c) git-story. Licensed under the GPL 3.0 License.
+ */
+
+export default {
+	'doit': 'Create a account to start writing!',
+	'email': 'Email',
+	'password': 'Password',
+	'password-check': 'Confirm Password',
+	'create-account': 'Create Account',
+	'agree-terms': 'By creating a account, you are agreeing to the <a href="/terms/service/" target="_blank" class="indigo--text text--darken-3">Terms of Service</a> for GIT STORY. You can refer the <a href="/terms/private/" target="_blank" class="indigo--text text--darken-3">Privacy Statement for GIT STORY</a> for a detailed GIT STORY Privacy Practices. Also, emails regarding your account will be sent when required.',
+	'connect-github': 'Sync Github Account',
+	'check-email': 'Confirm Email',
+};
