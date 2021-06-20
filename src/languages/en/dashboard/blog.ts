fa3ba7f0c272b70d6231503434253d6b5c0c23d3
ashzHax <ashkim2001@gmail.com>
2021-06-19 22:41:13 -0700

    en update

--- src/languages/en/dashboard/blog.ts
+++ src/languages/en/dashboard/blog.ts
@@ -8,4 +8,7 @@ export default {
 	'not-found-meta-data': 'Cannot find Git Story metadata.<br>Either it is not a repository made by Git Story, or the metadata has been removed.<br>Delete current repository and create a new repository?<br><br><b style="color: red;">Warning: All data will be gone from current repository.</b>',
 	'remove': 'Delete Post',
 	'remove-desc': 'Confirm deleting selected posts?<br>Cannot recover after deleting.',
+	'empty': 'No posts found. Write a new post!',
+	'search-post': 'Search Post',
+	'soon': 'In development',
 };
---
+++ src/languages/en/dashboard/setting/index.ts
@@ -0,0 +1,9 @@
+import menu from './menu';
+import blog from './blog';
+import profile from './profile';
+
+export default {
+	menu,
+	blog,
+	profile,
+};
